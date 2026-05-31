f9f5ec5d35c2825b41ce4e19a7d730c1304bcfde
kaihatsuyami <mana.kaihatsu.83@gmail.com>
2026-05-31 19:54:16 +0900

    vite.config.tsを修正

--- 620-birthday-app/vite.config.ts
+++ 620-birthday-app/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base:"/birthday620/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
