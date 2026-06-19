9d77ea5e06df2f055aaea0554b57461b386e2dba
manaikeuchi <mana.ikeuchi@g.softbank.co.jp>
2026-06-20 03:57:45 +0900

    全公開

---
+++ assets/SchedulePage-CzG6mBys.js
@@ -0,0 +1,6 @@
+import{a as e,g as t,h as n,n as r,o as i,p as a,s as o,u as s}from"./index-9BN0meNY.js";import{t as c}from"./AppHeader-DMhc91Y2.js";import{t as l}from"./ScheduleGroup-D2IsSvff.js";var u={class:`p-schedule`},d={__name:`SchedulePage`,setup(d){let f=r(),p=e=>{f.push(e)},m=[{date:`Day1`,items:[{time:`15:00`,event:`start / checkin
+＠Richmond Hotel Premier Schole`},{time:`xx:xx`,event:`Dinner
+＠Takeout or Delivery`},{time:`21:00`,event:`Nightcap
+＠Lounge`},{time:`22:00`,event:`The illumination of Tokyo Skytree
+＠Hotel 6th floor`}]},{date:`Day2`,items:[{time:`11:00`,event:`checkout`},{time:`11:30`,event:`Lunch
+＠secret...`}]}],h=i(()=>{let e=100;return m.map(t=>{let n=e;return e+=(t.items.length+2)*100,n})});return(r,i)=>(n(),s(`div`,u,[a(c,{variant:`schedule`}),(n(),s(e,null,t(m,(e,t)=>a(l,{key:e.date,date:e.date,items:e.items,"start-delay":h.value[t]},null,8,[`date`,`items`,`start-delay`])),64)),o(`p`,{class:`p-schedule__more`,onClick:i[0]||=e=>p(`/schedule/gate`)},`and more...`)]))}};export{d as default};
---
+++ assets/ScheduleSanrioPage-BgUUfwFk.js
@@ -0,0 +1,7 @@
+import{a as e,g as t,h as n,o as r,p as i,u as a}from"./index-9BN0meNY.js";import{t as o}from"./AppHeader-DMhc91Y2.js";import{t as s}from"./ScheduleGroup-D2IsSvff.js";var c={class:`p-schedule`},l={__name:`ScheduleSanrioPage`,setup(l){let u=[{date:`Day1`,items:[{time:`15:00`,event:`checkin
+＠Richmond Hotel Premier Schole`},{time:`xx:xx`,event:`Dinner
+＠Takeout or Delivery`},{time:`21:00`,event:`Nightcap
+＠Lounge`},{time:`22:00`,event:`The illumination of Tokyo Skytree
+＠Hotel 6th floor`}]},{date:`Day2`,items:[{time:`11:00`,event:`checkout`},{time:`11:30`,event:`Lunch
+＠sorairo`},{time:`13:00`,event:`oshiage to tama`},{time:`14:00`,event:`Enter sanriopuroland`},{time:`18:00`,event:`Exit sanriopuroland`},{time:`19:00`,event:`Dinner
+＠???`}]}],d=r(()=>{let e=100;return u.map(t=>{let n=e;return e+=(t.items.length+2)*100,n})});return(r,l)=>(n(),a(`div`,c,[i(o,{variant:`schedule`}),(n(),a(e,null,t(u,(e,t)=>i(s,{key:e.date,date:e.date,items:e.items,"start-delay":d.value[t]},null,8,[`date`,`items`,`start-delay`])),64))]))}};export{l as default};
